58a42a9f77dd75cacba11d86c9749044e4451436
Tom Georgelin <tom.georgelin@aviation-civile.gouv.fr>
2023-01-25 22:09:22 +0100

    feat(frontend): add VueJS frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<header>
+		<img
+			alt="petitepausecafe.fr logo"
+			class="logo"
+			src="./assets/logo.svg"
+			width="125"
+			height="125"
+		/>
+
+		<div class="wrapper"></div>
+	</header>
+
+	<main></main>
+</template>
+
+<script>
+export default {
+	name: 'app',
+	created() {
+		document.title = 'petitepausecafe.fr';
+	},
+};
+</script>
