3222c4ef09972d98ddc2f1db8c86d9f62e06a5a0
Tom Georgelin <tom.georgelin@gmail.com>
2023-04-21 21:06:02 +0200

    feat: add favicon

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -18,6 +18,17 @@ export default defineNuxtConfig({
 					content: 'Des articles concis et clairs !',
 				},
 			],
+			link: [
+				{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+				{
+					rel: 'apple-touch-icon',
+					sizes: '180x180',
+					href: '/apple-touch-icon.png',
+				},
+				{ rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
+				{ rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
+				{ rel: 'manifest', href: '/site.webmanifest' },
+			],
 		},
 	},
 	modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth'],
