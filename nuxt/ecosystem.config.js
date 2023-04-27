a21d533d73c44540ae611d64dfa920d00cc97803
Tom Georgelin <tom.georgelin@gmail.com>
2023-04-27 18:45:25 +0200

    fix: update ecosystem file

--- nuxt/ecosystem.config.js
+++ nuxt/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'petitepausecafe',
-			port: '443',
+			port: '80',
 			exec_mode: 'cluster',
 			instances: 'max',
 			script: './.output/server/index.mjs',
