97a3ba755d05e94babedafd07082fea835c302b6
Tom Georgelin <tom.georgelin@gmail.com>
2023-05-11 14:38:31 +0200

    fix: remove unused file

--- nuxt/pages/hello.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div v-if="articles.value != null">{{ articles }}</div>
-</template>
-<script>
-const { data: articles } = await useFetch('localhost:3000/api');
-export default {};
-</script>
